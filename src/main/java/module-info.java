fa53bff300fd6370a75552ca7ca1641fff73f5d8
Andrew Sills <andrew.k.sills@gmail.com>
2025-07-17 15:29:58 -0400

    removed method wraped service loading feature

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -10,16 +10,4 @@ module dev.ikm.tinkar.forge {
     exports dev.ikm.tinkar.forge.wrapper.record;
 
     uses dev.ikm.tinkar.forge.ForgeMethodWrapper;
-    provides dev.ikm.tinkar.forge.ForgeMethodWrapper with
-            //Methods based on calculators
-            dev.ikm.tinkar.forge.wrapper.of.AncestorsOf,
-            dev.ikm.tinkar.forge.wrapper.of.ChildrenOf,
-            dev.ikm.tinkar.forge.wrapper.of.ParentsOf,
-            dev.ikm.tinkar.forge.wrapper.of.DescendentsOf,
-            dev.ikm.tinkar.forge.wrapper.of.LatestVersionOf,
-            dev.ikm.tinkar.forge.wrapper.of.TextOf,
-            //Entity Service Component Lookup Methods
-            dev.ikm.tinkar.forge.wrapper.get.EntityGet,
-            //Specific Semantic Lookup Methods
-            dev.ikm.tinkar.forge.wrapper.on.DescriptionsOn;
 }
