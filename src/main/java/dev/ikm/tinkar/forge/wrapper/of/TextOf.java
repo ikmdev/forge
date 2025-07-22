caefacec2e612956ff0ad4082b3cb880a7369f2e
Greg Liebowitz <gliebowitz@deloitte.com>
2025-07-22 13:39:44 -0400

    IKDT-1074 Moved method into ForgeUtil

--- src/main/java/dev/ikm/tinkar/forge/wrapper/of/TextOf.java
+++ src/main/java/dev/ikm/tinkar/forge/wrapper/of/TextOf.java
@@ -4,6 +4,7 @@ import dev.ikm.tinkar.coordinate.language.calculator.LanguageCalculator;
 import dev.ikm.tinkar.entity.Entity;
 import dev.ikm.tinkar.entity.EntityVersion;
 import dev.ikm.tinkar.forge.ForgeMethodWrapper;
+import dev.ikm.tinkar.forge.ForgeUtil;
 import freemarker.ext.beans.GenericObjectModel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,20 +21,7 @@ public class TextOf extends ForgeMethodWrapper {
         GenericObjectModel languageCalcObjectModel = (GenericObjectModel) arguments.get(1);
         Entity<? extends EntityVersion> entity = convertGenericObjectModel(entityObjectModel, Entity.class);
         LanguageCalculator languageCalculator = convertGenericObjectModel(languageCalcObjectModel, LanguageCalculator.class);
-        return createDescription(entity, languageCalculator);
-    }
-
-    public String createDescription(Entity<? extends EntityVersion> entity, LanguageCalculator languageCalculator) {
-        String module = formatName(entity.versions().getAny().stamp().module().description());
-        String description = formatName(languageCalculator.getDescriptionTextOrNid(entity.nid()));
-        if (description.startsWith(module)) {
-            return description;
-        }
-        return module + "_" + description;
-    }
-
-    private String formatName(String name) {
-        return name.toUpperCase().replace("MODULE", "").replaceAll("[^A-Z0-9]", " ").trim().replaceAll("\\s+", "_");
+        return ForgeUtil.createBindingDescription(entity, languageCalculator);
     }
 
 }
